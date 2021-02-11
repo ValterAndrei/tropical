ef7722325d8646c88019fd8d6d8053d88f20773d
Valter <valter.sandoval@redealumni.com>
2021-02-11 11:16:54 -0300

    first commit

---
+++ lib/tropical/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Tropical
+  VERSION = "0.1.0"
+end
---
+++ spec/tropical_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Tropical do
+  it "has a version number" do
+    expect(Tropical::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
