ac1d0430d87d660cd6ffbaba56b58f602b8408ce
Valter <valter.sandoval@redealumni.com>
2021-02-11 11:17:18 -0300

    Add files

--- lib/tropical.rb
+++ lib/tropical.rb
@@ -1,8 +1,74 @@
-# frozen_string_literal: true
-
-require_relative "tropical/version"
+require 'date'
+require 'json'
+require 'net/http'
+require 'uri'
 
 module Tropical
-  class Error < StandardError; end
-  # Your code goes here...
+  class OpenWeatherMap
+    BASE_URL = 'https://api.openweathermap.org/data/2.5/forecast?'.freeze
+
+    attr_reader :data
+
+    def initialize(params)
+      request_params = build_request_params(params)
+      response       = post(request_params)
+
+      @data = JSON.parse(response.read_body)
+    end
+
+    def average_temp_by_days
+      group_by_days = organize_values.group_by { |item| item[:datetime].to_date }
+      days = []
+
+      group_by_days.each do |day, temps|
+        average = temps.sum { |time| time[:temp] } / temps.length
+
+        days << { day: day, average: average.round }
+      end
+
+      days
+    end
+
+    def current_date
+      organize_values.first[:datetime]
+    end
+
+    def current_temp
+      organize_values.first[:temp]
+    end
+
+    def current_weather
+      organize_values.first[:description]
+    end
+
+    private
+
+    def build_request_params(params)
+      link = ''
+
+      params.each do |k, v|
+        link += "&#{k}=#{v}" if v.is_a?(String) && !v.empty?
+      end
+
+      BASE_URL + link
+    end
+
+    def organize_values
+      data['list'].map do |list_item|
+        {
+          datetime: Time.at(list_item['dt']),
+          temp: list_item['main']['temp'],
+          description: list_item['weather'].first['description']
+        }
+      end
+    end
+
+    def post(request_params)
+      url          = URI(request_params)
+      http         = Net::HTTP.new(url.host, url.port)
+      http.use_ssl = true
+      request      = Net::HTTP::Get.new(url)
+      http.request(request)
+    end
+  end
 end
--- tropical.gemspec
+++ tropical.gemspec
@@ -3,21 +3,21 @@
 require_relative "lib/tropical/version"
 
 Gem::Specification.new do |spec|
-  spec.name          = "tropical"
-  spec.version       = Tropical::VERSION
-  spec.authors       = ["Valter"]
-  spec.email         = ["valter.sandoval@redealumni.com"]
+  spec.name        = "tropical"
+  spec.version     = Tropical::VERSION
+  spec.authors     = ["Valter"]
+  spec.email       = ["valterandrey@gmail.com"]
 
-  spec.summary       = "TODO: Write a short summary, because RubyGems requires one."
-  spec.description   = "TODO: Write a longer description or delete this line."
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
+  spec.summary     = "Weather forecast query"
+  spec.description = "Consultation weather forecast up to 5 days."
+  spec.homepage    = "https://github.com/ValterAndrei/tropical"
   spec.required_ruby_version = Gem::Requirement.new(">= 2.4.0")
 
-  spec.metadata["allowed_push_host"] = "TODO: Set to 'http://mygemserver.com'"
+  spec.metadata["allowed_push_host"] = "https://rubygems.org/gems/tropical"
 
   spec.metadata["homepage_uri"] = spec.homepage
-  spec.metadata["source_code_uri"] = "TODO: Put your gem's public repo URL here."
-  spec.metadata["changelog_uri"] = "TODO: Put your gem's CHANGELOG.md URL here."
+  spec.metadata["source_code_uri"] = "https://github.com/ValterAndrei/tropical"
+  spec.metadata["changelog_uri"] = "https://github.com/ValterAndrei/tropical"
 
   # Specify which files should be added to the gem when it is released.
   # The `git ls-files -z` loads the files in the RubyGem that have been added into git.
