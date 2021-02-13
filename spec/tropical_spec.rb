7e729b163c004269f822736f08e08c3ec504f0f7
Valter Andrei <valterandrey@gmail.com>
2021-02-13 13:21:24 -0300

    Refactor specs

--- spec/tropical_spec.rb
+++ spec/tropical_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe Tropical::OpenWeatherMap do
       expect(response).to receive(:body) { response_body }
     end
 
-    let(:response_body) { File.read(FILE_SUCCESS_RESPONSE) }
+    let(:response_body) { File.read(File.expand_path("spec/fixtures/response.json")) }
     let(:params) { { appid: "valid_appid", q: "caconde,br", lang: "pt_br", units: "metric" } }
 
     subject { described_class.new(params) }
@@ -62,13 +62,47 @@ RSpec.describe Tropical::OpenWeatherMap do
         expect(subject.current_weather).to eq("chuva moderada")
       end
     end
+
+    it "return status" do
+      expect(subject.status).to eq("200")
+    end
   end
 
   describe "when the request is unauthorized" do
+    before do
+      response = Net::HTTPUnauthorized.new(1.0, "401", "Unauthorized")
+      expect_any_instance_of(Net::HTTP).to receive(:request) { response }
+    end
+
+    let(:params) { { appid: "invalid_appid", q: "caconde,br", lang: "pt_br", units: "metric" } }
+
+    subject { described_class.new(params) }
+
+    it "return correct values" do
+      expect(subject.data).to eq({ error: "Unauthorized: appid is invalid." })
+    end
+
+    it "return status" do
+      expect(subject.status).to eq("401")
+    end
   end
 
-  describe "when the request has an error" do
+  describe "when the request has an error or not found city" do
+    before do
+      response = Net::HTTPServerError.new(1.0, "404", "Not Found")
+      expect_any_instance_of(Net::HTTP).to receive(:request) { response }
+    end
+
+    let(:params) { { appid: "valid_appid", q: "anyway,br" } }
+
+    subject { described_class.new(params) }
+
+    it "return correct values" do
+      expect(subject.data).to eq({ error: "Not Found" })
+    end
+
+    it "return status" do
+      expect(subject.status).to eq("404")
+    end
   end
 end
-
-FILE_SUCCESS_RESPONSE = File.expand_path("spec/fixtures/success_response.json")
