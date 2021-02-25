d9924ab24f56b8110c94e3559ba4508f5376ae1d
Valter <valter.sandoval@redealumni.com>
2021-02-25 18:15:19 -0300

    Fix ci

--- spec/tropical_spec.rb
+++ spec/tropical_spec.rb
@@ -19,16 +19,16 @@ RSpec.describe Tropical::OpenWeatherMap do
       it "return correct values" do
         expect(subject.send(:list)).to eq(
           [
-            { dt: Time.new(2021, 2, 13, 15).utc, temp: 21.86, description: "chuva moderada" },
-            { dt: Time.new(2021, 2, 13, 18).utc, temp: 21.46, description: "chuva leve" },
-            { dt: Time.new(2021, 2, 13, 21).utc, temp: 19.04, description: "chuva leve" },
-            { dt: Time.new(2021, 2, 14,  0).utc, temp: 18.18, description: "nublado" },
-            { dt: Time.new(2021, 2, 14,  3).utc, temp: 17.51, description: "nublado" },
-            { dt: Time.new(2021, 2, 14,  6).utc, temp: 17.1,  description: "nublado" },
-            { dt: Time.new(2021, 2, 14,  9).utc, temp: 22.44, description: "nublado" },
-            { dt: Time.new(2021, 2, 14, 12).utc, temp: 25.16, description: "chuva leve" },
-            { dt: Time.new(2021, 2, 14, 15).utc, temp: 26,    description: "chuva leve" },
-            { dt: Time.new(2021, 2, 14, 18).utc, temp: 23.34, description: "chuva leve" }
+            { dt: Time.new(2021, 2, 13, 15), temp: 21.86, description: "chuva moderada" },
+            { dt: Time.new(2021, 2, 13, 18), temp: 21.46, description: "chuva leve" },
+            { dt: Time.new(2021, 2, 13, 21), temp: 19.04, description: "chuva leve" },
+            { dt: Time.new(2021, 2, 14,  0), temp: 18.18, description: "nublado" },
+            { dt: Time.new(2021, 2, 14,  3), temp: 17.51, description: "nublado" },
+            { dt: Time.new(2021, 2, 14,  6), temp: 17.1,  description: "nublado" },
+            { dt: Time.new(2021, 2, 14,  9), temp: 22.44, description: "nublado" },
+            { dt: Time.new(2021, 2, 14, 12), temp: 25.16, description: "chuva leve" },
+            { dt: Time.new(2021, 2, 14, 15), temp: 26,    description: "chuva leve" },
+            { dt: Time.new(2021, 2, 14, 18), temp: 23.34, description: "chuva leve" }
           ]
         )
       end
