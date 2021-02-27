740cdab029fb2b14726b765a08e1e3c97741827f
Valter Andrei <valterandrey@gmail.com>
2021-02-27 13:48:15 -0300

    Fix ci

--- spec/tropical_spec.rb
+++ spec/tropical_spec.rb
@@ -19,16 +19,16 @@ RSpec.describe Tropical::OpenWeatherMap do
       it "return correct values" do
         expect(subject.send(:list)).to eq(
           [
-            { dt: Time.new(2021, 2, 13, 18).in_time_zone("America/Sao_Paulo"), temp: 21.86, description: "chuva moderada" },
-            { dt: Time.new(2021, 2, 13, 21).in_time_zone("America/Sao_Paulo"), temp: 21.46, description: "chuva leve" },
-            { dt: Time.new(2021, 2, 13,  0).in_time_zone("America/Sao_Paulo"), temp: 19.04, description: "chuva leve" },
-            { dt: Time.new(2021, 2, 14,  3).in_time_zone("America/Sao_Paulo"), temp: 18.18, description: "nublado" },
-            { dt: Time.new(2021, 2, 14,  6).in_time_zone("America/Sao_Paulo"), temp: 17.51, description: "nublado" },
-            { dt: Time.new(2021, 2, 14,  9).in_time_zone("America/Sao_Paulo"), temp: 17.1,  description: "nublado" },
-            { dt: Time.new(2021, 2, 14, 12).in_time_zone("America/Sao_Paulo"), temp: 22.44, description: "nublado" },
-            { dt: Time.new(2021, 2, 14, 15).in_time_zone("America/Sao_Paulo"), temp: 25.16, description: "chuva leve" },
-            { dt: Time.new(2021, 2, 14, 18).in_time_zone("America/Sao_Paulo"), temp: 26,    description: "chuva leve" },
-            { dt: Time.new(2021, 2, 14, 21).in_time_zone("America/Sao_Paulo"), temp: 23.34, description: "chuva leve" }
+            { dt: Time.new(2021, 2, 13, 15), temp: 21.86, description: "chuva moderada" },
+            { dt: Time.new(2021, 2, 13, 18), temp: 21.46, description: "chuva leve" },
+            { dt: Time.new(2021, 2, 13, 21), temp: 19.04, description: "chuva leve" },
+            { dt: Time.new(2021, 2, 14,  0), temp: 18.18, description: "nublado" },
+            { dt: Time.new(2021, 2, 14,  3), temp: 17.51, description: "nublado" },
+            { dt: Time.new(2021, 2, 14,  6), temp: 17.1,  description: "nublado" },
+            { dt: Time.new(2021, 2, 14,  9), temp: 22.44, description: "nublado" },
+            { dt: Time.new(2021, 2, 14, 12), temp: 25.16, description: "chuva leve" },
+            { dt: Time.new(2021, 2, 14, 15), temp: 26,    description: "chuva leve" },
+            { dt: Time.new(2021, 2, 14, 18), temp: 23.34, description: "chuva leve" }
           ]
         )
       end
@@ -38,7 +38,7 @@ RSpec.describe Tropical::OpenWeatherMap do
       it "return correct values" do
         expect(subject.send(:average_temp_by_days)).to eq(
           [
-            { day: Date.new(2021, 2, 13), average: 21 },
+            { day: Date.new(2021, 2, 13), average: 22 },
             { day: Date.new(2021, 2, 14), average: 21 }
           ]
         )
@@ -134,12 +134,12 @@ RSpec.describe Tropical::OpenWeatherMap do
 
       it "return correct value" do
         expect(subject.sumary_days_forecast).to eq(
-          "21°C em 13/02, "\
+          "22°C em 13/02, "\
           "21°C em 14/02, "\
           "22°C em 15/02, "\
           "22°C em 16/02, "\
           "22°C em 17/02 e "\
-          "22°C em 18/02."
+          "21°C em 18/02."
         )
       end
     end
@@ -151,12 +151,12 @@ RSpec.describe Tropical::OpenWeatherMap do
         expect(subject.full_sumary).to eq(
           "23°C e chuva moderada em Caconde em 13/02. "\
           "Média para os próximos dias: "\
-          "21°C em 13/02, "\
+          "22°C em 13/02, "\
           "21°C em 14/02, "\
           "22°C em 15/02, "\
           "22°C em 16/02, "\
           "22°C em 17/02 e "\
-          "22°C em 18/02."
+          "21°C em 18/02."
         )
       end
     end
